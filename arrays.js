a31c1f7d1cf254d48109a3303e42a212dbacdbd3
Mary Varnado <myvarnado@gmail.com>
2020-01-10 18:25:48 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+  array.shift
+  return array
+}
