b080c2c54ce464faad13bd42086cf2f31bbd5d31
Mary Varnado <myvarnado@gmail.com>
2020-01-10 18:31:22 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array.slice(1)
+  array = array.slice(1)
   return array
 }
 
