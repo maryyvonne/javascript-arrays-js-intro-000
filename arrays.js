271511d1edd34a7f14580f9160e2429656d493d9
Mary Varnado <myvarnado@gmail.com>
2020-01-10 17:28:39 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,4 @@ var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`]
 function addElementToBeginningOfArray(array, element) {
   var x = array.unshift(element); return (array[0]);
 }
-addElementToBeginningOfArray([1], "foo")
+addElementToBeginningOfArray(1, "foo")
