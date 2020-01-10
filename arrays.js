beec92a0d9434b29f67f5563cd187d9d5c7f2177
Mary Varnado <myvarnado@gmail.com>
2020-01-10 18:36:28 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,6 +38,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
   return array
 }
 function removeElementFromEndOfArray (array) {
-  array.pop()
+  array.slice(0, array.lenth - 1)
   return array
 }
